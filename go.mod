0d97f6e98cff2996e66e11c111ba421fadbc5cbe
sadegh <adyani.sadegh@gmail.com>
2024-02-07 02:02:14 +0330

    initialize project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wind-direction
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	windRegexp    = regexp.MustCompile(`\d* METAR.*EGLL \d*Z [A-Z ]*(\d{5}KT|VRB\d{2}KT).*=`)
+	tafValidation = regexp.MustCompile(`.*TAF.*`)
+	comment       = regexp.MustCompile(`\w*#.*`)
+	metarClose    = regexp.MustCompile(`.*=`)
+	variableWind  = regexp.MustCompile(`.*VRB\d{2}KT`)
+	validWind     = regexp.MustCompile(`\d{5}KT`)
+	windDirOnly   = regexp.MustCompile(`(\d{3})\d{2}KT`)
+	windDist      [8]int
+)
+
+func parseToArray(text string) []string {
+	lines := strings.Split(text, "\n")
+	metarSlice := make([]string, 0, len(lines))
+	metarStr := ""
+
+	for _, line := range lines {
+		if tafValidation.MatchString(line) {
+			break
+		}
+		if !comment.MatchString(line) {
+			metarStr += strings.Trim(line, " ")
+		}
+		if metarClose.MatchString(line) {
+			metarSlice = append(metarSlice, metarStr)
+			metarStr = ""
+		}
+	}
+
+	return metarSlice
+}
+
+func extractWindDirection(metars []string) []string {
+	winds := make([]string, 0, len(metars))
+
+	for _, metar := range metars {
+		if windRegexp.MatchString(metar) {
+			winds = append(winds, windRegexp.FindAllStringSubmatch(metar, -1)[0][1])
+		}
+	}
+	return winds
+}
+
+func mineWindDistribution(winds []string) {
+	for _, wind := range winds {
+		if variableWind.MatchString(wind) {
+			for i := 0; i < 8; i++ {
+				windDist[i]++
+			}
+		} else if validWind.MatchString(wind) {
+			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+				dirIndex := int(math.Round(d/45.0)) % 8
+				windDist[dirIndex]++
+			}
+		}
+	}
+}
+
+func main() {
+	absPath, _ := filepath.Abs("/Users/sadegh/Code/Golang/wind-direction/metarfiles")
+	files, _ := os.ReadDir(absPath)
+	start := time.Now()
+
+	for _, file := range files {
+		dat, err := os.ReadFile(filepath.Join(absPath, file.Name()))
+		if err != nil {
+			panic(err)
+		}
+
+		text := string(dat)
+
+		metarsRepost := parseToArray(text)
+
+		windDirections := extractWindDirection(metarsRepost)
+
+		mineWindDistribution(windDirections)
+	}
+
+	elapsed := time.Since(start)
+
+	fmt.Printf("%v\n", windDist)
+	fmt.Printf("Proccessing toke %s\n", elapsed)
+}
